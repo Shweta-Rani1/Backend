7e5666cf95f2915c01191f221053ed0f773e0f6e
Shweta <2002shwetarani@gmail.com>
2023-07-23 23:52:09 +0530

    added mongoose

--- mongoosejs/src/app.js
+++ mongoosejs/src/app.js
@@ -3,4 +3,3 @@ const mongoose=require("mongoose");
 mongoose.connect("mongodb://0.0.0.0:27017/mngodbtutorial", {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log("DB Connetion Successfull" ))
   .catch((err) =>  console.log(err.message));
-
